a5eeb624ac1fe7480e6ef5f1af924ac9a1cd836b
Brian Butcher <butcher.brian.c@gmail.com>
2019-12-01 21:56:40 -0500

    removed testing folder

--- Testing/index.js
+++
@@ -1,8 +0,0 @@
-const app = require('commander');
-
-app
-.version('1.0.0')
-.option('-e, --environment <environment>', 'Environment to work with')
-.action(options => {
-    console.log(options.environment);
-});
